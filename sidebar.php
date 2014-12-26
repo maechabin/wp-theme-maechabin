f224bd5aaff8efe456a15223e915877b7a0ffbcf
maechabin <mae@chab.in>
2014-12-27 01:39:12 +0900

    fix responsive

--- sidebar.php
+++ sidebar.php
@@ -2,7 +2,7 @@
 <div id="sidebar_sub">
 
 <aside>
-<div>
+<div style="text-align: center;">
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <!-- mae's blog（サイドバー上） -->
 <ins class="adsbygoogle"
@@ -29,13 +29,13 @@
 </p>
 
 <p class="author-description">
-<a href="http://maechabin.tumblr.com" target="_blank"><img src="/wp-content/uploads/2013/11/maes-works.png" width="298" height="49" alt="写真ブログ mae's works - maechabin.tumblr.com"></a>
-<a href="http://maechabin.tumblr.com" target="_blank">mae's works</a>。私の写真公開用ブログです。写真が趣味で日々撮った写真をアップしています。使用しているTumblrも全力でカスタマイズ中！
+<a href="//maechabin.tumblr.com" target="_blank"><img src="/wp-content/uploads/2013/11/maes-works.png" width="298" height="49" alt="写真ブログ mae's works - maechabin.tumblr.com"></a>
+<a href="//maechabin.tumblr.com" target="_blank">mae's works</a>。私の写真公開用ブログです。写真が趣味で日々撮った写真をアップしています。使用しているTumblrも全力でカスタマイズ中！
 </p>
 
 <?php if(!is_home()): ?>
 <div class="twitter-timeline-area">
-<a class="twitter-timeline" href="https://twitter.com/maechabin" data-widget-id="510042758585651200">@maechabin からのツイート</a>
+<a class="twitter-timeline" href="//twitter.com/maechabin" data-widget-id="510042758585651200">@maechabin からのツイート</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 </div>
 <?php endif; ?>
@@ -49,14 +49,14 @@
 <div class="title">Others</div>
 <div class="textwidget">
 <figure class="bottom_0">
-<a href="http://feedmeter.net/detail.php?r=http%3A%2F%2Fmae.chab.in%2Ffeed" target="_blank"><img src="http://feedmeter.net/image.php?r=http%3A%2F%2Fmae.chab.in%2Ffeed" width="60" height="36" alt="フィードメーター - Mae's Blog" /></a>
-<a href="http://www.blogmura.com/" target="_blank"><img src="http://www.blogmura.com/img/www88_31.gif" width="88" height="31" alt="ブログランキング・にほんブログ村へ" /></a>
-<a href="http://blogram.jp/users/agent/vote/?uid=116487#ref=vote" target="_blank"><img src="http://widget.blogram.jp/images/bgButton1_whi.gif" alt="blogram投票ボタン" width="88" height="31"  /></a><br />
-<a href="http://blog.with2.net/link.php?1028897" target="_blank"><img src="http://image.with2.net/img/banner/banner_13.gif" width="80" height="15" alt="人気ブログランキングへ" /></a>
+<a href="//feedmeter.net/detail.php?r=http%3A%2F%2Fmae.chab.in%2Ffeed" target="_blank"><img src="//feedmeter.net/image.php?r=http%3A%2F%2Fmae.chab.in%2Ffeed" width="60" height="36" alt="フィードメーター - Mae's Blog"></a>
+<a href="//www.blogmura.com/" target="_blank"><img src="//www.blogmura.com/img/www88_31.gif" width="88" height="31" alt="ブログランキング・にほんブログ村へ"></a>
+<a href="//blogram.jp/users/agent/vote/?uid=116487#ref=vote" target="_blank"><img src="//widget.blogram.jp/images/bgButton1_whi.gif" alt="blogram投票ボタン" width="88" height="31"></a>
+<a href="//blog.with2.net/link.php?1028897" target="_blank"><img src="//image.with2.net/img/banner/banner_13.gif" width="80" height="15" alt="人気ブログランキングへ"></a>
 </figure>
 </div>
 
-<div style="margin-bottom:18px;">
+<div style="margin-bottom: 18px;text-align: center;">
 
 <!--    
 <a href="http://px.a8.net/svt/ejp?a8mat=1U7TPO+6EESNU+D8Y+C3BAP" target="_blank">
