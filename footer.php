3ad24ec49a30f95145999ac0597a9ca5c0a9baff
maechabin <mail@chab.in>
2015-06-12 01:56:34 +0900

    modified footer style

--- footer.php
+++ footer.php
@@ -1,30 +1,55 @@
-<!-- ▼footer▼ -->
-<footer>
-<div class="footer">
-<div id="footer_sub" class="footer__sub">
+	<!-- ▼footer▼ -->
+	<footer>
+		<div class="footer">
+			<div id="footer_sub" class="footer__sub">
 
-<address class="footer__credit">
-	Copyright &copy; <a href="<?php echo home_url( '/' ) ?>">maechabin</a>
-	<a title="Googleプロフィールへ" href="https://plus.google.com/105642680110503345013?rel=author" target="_blank"><img src="//ssl.gstatic.com/images/icons/gplus-16.png" alt="Googleプロフィールへ" height="16" width="16"></a>
-</address>
+				<ul class="footer__share-button">
+					<li class="footer__share-button--list">
+						<a href="http://www.facebook.com/maechabin" target="_blank" class="footer__share-button--fb">
+							<i class="fa fa-facebook"></i>
+						</a>
+					<li>
+					<li class="footer__share-button--list">
+						<a href="https://twitter.com/maechabin" target="_blank" class="footer__share-button--tw">
+							<i class="fa fa-twitter"></i>
+						</a>
+					</li>
+					<li class="footer__share-button--list">
+						<a href="https://plus.google.com/105642680110503345013?rel=author" target="_blank" class="footer__share-button--gp">
+							<i class="fa fa-google-plus"></i>
+						</a>
+					</li>
+				</ul>
 
-</div><!-- #footer_sub -->
-</div>
+				<address class="footer__credit">
+					Copyright &copy; <a href="/">maechabin</a>
+				</address>
 
-<div id="footer_bar" class="footer__bar">
-	<ul>
-		<li class="footer__bar-link footer__bar-link--left footer__style--hidden" id="footer__bar__agenda-link"><a href="#agenda"><i class="fa fa-chevron-circle-up"></i>目次に戻る</a></li>
-		<li class="footer__bar-link footer__bar-link--right"><a href="#TOP"><i class="fa fa-chevron-circle-up"></i>ページ上部に戻る</a></li>
-	</ul>
-</div>
+			</div><!-- #footer_sub -->
+		</div>
 
-</footer>
-<!-- ▲footer▲ -->
+		<div id="footer_bar" class="footer__bar">
+			<ul>
+				<li class="footer__bar-link footer__bar-link--left footer__style--hidden" id="footer__bar__agenda-link">
+					<a href="#agenda">
+						<i class="fa fa-chevron-circle-up"></i>目次に戻る
+					</a>
+				</li>
+				<li class="footer__bar-link footer__bar-link--right">
+					<a href="#TOP">
+						<i class="fa fa-chevron-circle-up"></i>ページ上部に戻る
+					</a>
+				</li>
+			</ul>
+		</div>
+
+	</footer>
+	<!-- ▲footer▲ -->
 
 </div><!-- #index -->
 
 <?php wp_footer(); ?>
-<?php if(is_single()): ?>
+<?php if (is_single()): ?>
 <script src="/wp-includes/js/comment-reply.js?ver=20090102" defer></script>
 <script src="//google-code-prettify.googlecode.com/svn/trunk/src/prettify.js"></script>
 <script src="//google-code-prettify.googlecode.com/svn/trunk/src/lang-css.js"></script>
--- header.php
+++ header.php
@@ -1,49 +1,50 @@
-<!DOCTYPE html>
+<!doctype html>
 
 <html lang="ja">
 <head>
-<meta charset="utf-8">
-<!--[if lt IE 9]>
-<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-<![endif]-->
-<title><?php site_title() ?></title>
-<meta http-equiv="default-style" content="<?php bloginfo('stylesheet_url'); ?>">
-<meta name="description" content="都内のIT企業でWebディレクターをやってます。HTML5、CSS3、JavaScript、jQueryなどのWeb技術に関する話題から、Apple製品のこと、音楽（ピアノ、作曲）のことなどを書いています。">
-<meta name="keywords" content="JavaScript, jQuery, HTML5, wordpress, apple">
-<meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1">
-<link rel="shortcut icon" type="image/vnd.microsoft.icon" href="/wp-content/themes/chabin/favicon.ico">
-<link rel="icon" type="image/vnd.microsoft.icon" href="/wp-content/themes/chabin/favicon.ico">
-<link rel="stylesheet" href="/wp-content/themes/chabin/assets/style-a8737afcb9f1aebab1b25c2a5ce7e5d9.css" media="all">
-<?php /*<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Play&amp;text=mae's+blog">*/ ?>
-<?php /*<link rel="stylesheet" href="/wp-content/themes/chabin/css/prettify.min.css">*/ ?>
-<?php /*<link rel="stylesheet" href="/wp-content/themes/chabin/css/font-awesome.min.css">*/ ?>
-<link rel="alternate" type="application/rss+xml" title="RSS" href="<?php bloginfo('rdf_url'); ?>">
-<?php get_template_part('header-custom-field'); ?>
-<?php wp_head(); ?>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+  <meta charset="utf-8">
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+  <title><?php site_title() ?></title>
+  <meta http-equiv="default-style" content="<?php bloginfo('stylesheet_url'); ?>">
+  <meta name="description" content="JavaScript、HTML5、CSS3などのフロントエンド関連のWeb技術の話題から、Apple製品のこと、音楽（ピアノ、作曲）のことなどを書いています。">
+  <meta name="keywords" content="JavaScript, jQuery, HTML5, wordpress, apple">
+  <meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1">
+  <link rel="shortcut icon" type="image/vnd.microsoft.icon" href="/wp-content/themes/chabin/favicon.ico">
+  <link rel="icon" type="image/vnd.microsoft.icon" href="/wp-content/themes/chabin/favicon.ico">
+  <link rel="stylesheet" href="/wp-content/themes/chabin/assets/style-ca7418f8632a109b5bbdab01f680aaf5.css" media="all">
+  <?php /*<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Play&amp;text=mae's+blog">*/ ?>
+  <?php /*<link rel="stylesheet" href="/wp-content/themes/chabin/css/prettify.min.css">*/ ?>
+  <?php /*<link rel="stylesheet" href="/wp-content/themes/chabin/css/font-awesome.min.css">*/ ?>
+  <link rel="alternate" type="application/rss+xml" title="RSS" href="<?php bloginfo('rdf_url'); ?>">
+  <?php get_template_part('header-custom-field'); ?>
+  <?php wp_head(); ?>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 </head>
 
 <body id="TOP">
-<div id="index">
+  <div id="index">
 
-<!-- ▼header▼ -->
-<header id="header_bar" class="header">
-<div id="header_bar_inner" class="header__inner">
+    <!-- ▼header▼ -->
+    <header id="header_bar" class="header">
+      <div id="header_bar_inner" class="header__inner">
 
-<h1 class="header__title"><a href="/"><i style="color:#fff;"></i>mae's blog</a></h1>
+        <h1 class="header__title"><a href="/"><i style="color:#fff;"></i>mae's blog</a></h1>
 
-<div class="header__search">
-  <form role="search" method="get" id="header__search-form" action="<?php bloginfo('url') ?>" >
-    <input type="text" value="<?php echo get_search_query(); ?>" name="s" placeholder="ブログを検索" class="header__search-text"><button type="submit" class="header__search-button"><i class="fa fa-search"></i></button>
-  </form>
-</div>
+        <div class="header__search">
+          <form role="search" method="get" id="header__search-form" action="<?php bloginfo('url') ?>" >
+            <input type="text" value="<?php echo get_search_query(); ?>" name="s" placeholder="ブログを検索" class="header__search-text"><button type="submit" class="header__search-button"><i class="fa fa-search"></i></button>
+          </form>
+        </div>
 
-<div class="header__share">
-  <a href="//cloud.feedly.com/#subscription%2Ffeed%2Fhttp%3A%2F%2Fmae.chab.in%2Ffeed" target="_blank"><img id="feedlyFollow" src="/wp-content/themes/chabin/image/feedly.png" alt="follow us in feedly" width="71" height="28"></a>
-  <a href="/feed" target="_blank"><i class="fa fa-rss"></i></a>
-</div>
+        <div class="header__share">
+          <a href="//cloud.feedly.com/#subscription%2Ffeed%2Fhttp%3A%2F%2Fmae.chab.in%2Ffeed" target="_blank"><img id="feedlyFollow" src="/wp-content/themes/chabin/image/feedly.png" alt="follow us in feedly" width="71" height="28"></a>
+          <a href="/feed" target="_blank"><i class="fa fa-rss"></i></a>
+        </div>
 
-</div>
-</header>
-<div id="blog_header"></div>
-<!-- ▲header▲ -->
+      </div>
+    </header>
+    <!-- ▲header▲ -->
+
+    <div id="blog_header"></div>
