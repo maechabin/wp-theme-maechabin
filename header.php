3a503f032cb0473e8aab798fd25aeda373d97ef6
maechabin <mail@chab.in>
2015-06-14 04:10:03 +0900

    change the page-nav style

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
   <meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1">
   <link rel="shortcut icon" type="image/vnd.microsoft.icon" href="/wp-content/themes/chabin/favicon.ico">
   <link rel="icon" type="image/vnd.microsoft.icon" href="/wp-content/themes/chabin/favicon.ico">
-  <link rel="stylesheet" href="/wp-content/themes/chabin/assets/style-ca7418f8632a109b5bbdab01f680aaf5.css" media="all">
+  <link rel="stylesheet" href="/wp-content/themes/chabin/assets/style-a28ba4bc76c76d98c44a80d690904083.css" media="all">
   <?php /*<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Play&amp;text=mae's+blog">*/ ?>
   <?php /*<link rel="stylesheet" href="/wp-content/themes/chabin/css/prettify.min.css">*/ ?>
   <?php /*<link rel="stylesheet" href="/wp-content/themes/chabin/css/font-awesome.min.css">*/ ?>
