baacec19fde6acdb5f4cc368d1568916e229eeb0
maechabin <mail@chab.in>
2015-06-14 00:22:21 +0900

    modified sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,70 +1,72 @@
 <div id="sidebar" class="sidebar">
-<div id="sidebar_sub" class="sidebar__sub">
+  <div id="sidebar_sub" class="sidebar__sub">
 
-<aside>
-<div class="sidebar__widget_ad">
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- mae's blog（サイドバー上） -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:300px;height:250px"
-     data-ad-client="ca-pub-6331923403728737"
-     data-ad-slot="3530398982"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-</div>
-</aside>
+    <aside>
+      <div class="sidebar__widget_ad">
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <!-- mae's blog（サイドバー上） -->
+        <ins class="adsbygoogle"
+          style="display:inline-block;width:300px;height:250px"
+          data-ad-client="ca-pub-6331923403728737"
+          data-ad-slot="3530398982"></ins>
+        <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+      </div>
+    </aside>
 
-<div class="sidebar__title">About Me</div>
-<p class="sidebar__author-name">Takanori Maeda
-  <a href="//twitter.com/maechabin" target="_blank" onclick="ga('send', 'event', 'sidebar', 'click', 'about-twitter');"><i class="fa fa-twitter"></i></a>
-  <a href="//www.facebook.com/maechabin" target="_blank" onclick="ga('send', 'event', 'sidebar', 'click', 'about-facebook');"><i class="fa fa-facebook"></i></a>
-  <a href="//plus.google.com/+TakanoriMaeda0_0/" target="_blank" onclick="ga('send', 'event', 'sidebar', 'click', 'about-google+');"><i class="fa fa-google-plus"></i></a>
-  <a href="//maechabin.tumblr.com/" target="_blank" onclick="ga('send', 'event', 'sidebar', 'click', 'about-tumblr');"><i class="fa fa-tumblr"></i></a>
-  <a href="//github.com/maechabin" target="_blank" onclick="ga('send', 'event', 'sidebar', 'click', 'about-github');"><i class="fa fa-github-alt"></i></a>
-  <a href="//jsdo.it/maechabin" target="_blank" onclick="ga('send', 'event', 'sidebar', 'click', 'about-jsdoit');"><i class="fa fa-html5"></i></a>
-  <a href="//www.muzie.ne.jp/artist/a024945/" target="_blank" onclick="ga('send', 'event', 'sidebar', 'click', 'about-muzie');"><i class="fa fa-music"></i></a>
-</p>
-<p class="sidebar__author-description">プログラマー、デザイナーなどを経て現在は都内のネット広告会社でマーケティングをやっています。JavaScriptやHTML5/CSS3などフロントエンドの技術に興味あり。専門はUI/UXデザイン、SEOなど。趣味はカメラ、ピアノ。</p>
-<p class="sidebar__author-description">
-<a href="//maechabin.tumblr.com" target="_blank"><img src="/wp-content/uploads/2013/11/maes-works.png" width="298" height="49" alt="写真ブログ Nikon[D5300/ニコワン]ユーザー mae's works"></a>
-私の写真公開用ブログです。写真が趣味で日々撮った写真をアップしています。使用しているTumblrも全力でカスタマイズ中！
-</p>
+    <div class="sidebar__title">About Me</div>
+    <p class="sidebar__author-name">Takanori Maeda
+      <a href="//twitter.com/maechabin" target="_blank" onclick="ga('send', 'event', 'sidebar', 'click', 'about-twitter');"><i class="fa fa-twitter"></i></a>
+      <a href="//www.facebook.com/maechabin" target="_blank" onclick="ga('send', 'event', 'sidebar', 'click', 'about-facebook');"><i class="fa fa-facebook"></i></a>
+      <a href="//plus.google.com/+TakanoriMaeda0_0/" target="_blank" onclick="ga('send', 'event', 'sidebar', 'click', 'about-google+');"><i class="fa fa-google-plus"></i></a>
+      <a href="//maechabin.tumblr.com/" target="_blank" onclick="ga('send', 'event', 'sidebar', 'click', 'about-tumblr');"><i class="fa fa-tumblr"></i></a>
+      <a href="//github.com/maechabin" target="_blank" onclick="ga('send', 'event', 'sidebar', 'click', 'about-github');"><i class="fa fa-github-alt"></i></a>
+      <a href="//jsdo.it/maechabin" target="_blank" onclick="ga('send', 'event', 'sidebar', 'click', 'about-jsdoit');"><i class="fa fa-html5"></i></a>
+      <a href1="//www.muzie.ne.jp/artist/a024945/" target="_blank" onclick="ga('send', 'event', 'sidebar', 'click', 'about-muzie');"><i class="fa fa-music"></i></a>
+    </p>
+    <p class="sidebar__author-description">プログラマー、デザイナーなどを経て現在は都内のネット広告会社でマーケティングをやっています。JavaScriptやHTML5/CSS3などフロントエンドの技術に興味あり。専門はUI/UXデザイン、SEOなど。趣味はカメラ、ピアノ。</p>
+    <figure class="sidebar__author-description">
+      <a href="//maechabin.tumblr.com" target="_blank">
+        <img src="/wp-content/uploads/2013/11/maes-works.png" width="298" height="49" alt="写真ブログ Nikon[D5300/ニコワン]ユーザー mae's works">
+      </a>
+      <figcaption>私の写真ブログです。写真が趣味で日々撮った写真をアップしています。使用しているTumblrも全力でカスタマイズ中！</figcaption>
+    </figure>
 
-<?php if(!is_home()): ?>
-<div class="sidebar__widget_twitter">
-<a class="twitter-timeline" href="//twitter.com/maechabin" data-widget-id="510042758585651200">@maechabin からのツイート</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-</div>
-<?php endif; ?>
+    <?php if(!is_home()): ?>
+      <div class="sidebar__widget_twitter">
+        <a class="twitter-timeline" href="//twitter.com/maechabin" data-widget-id="510042758585651200">
+          @maechabin からのツイート
+        </a>
+        <script>
+          !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+        </script>
+      </div>
+    <?php endif; ?>
 
-<nav>
-<?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar() ): ?>
-<?php endif; ?>
-</nav>
+    <nav>
+    <?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar() ): ?>
+    <?php endif; ?>
+    </nav>
 
-<aside>
-<div class="sidebar__title">Others</div>
-<figure>
-<a href="//feedmeter.net/detail.php?r=http%3A%2F%2Fmae.chab.in%2Ffeed" target="_blank"><img src="//feedmeter.net/image.php?r=http%3A%2F%2Fmae.chab.in%2Ffeed" width="60" height="36" alt="フィードメーター - Mae's Blog"></a>
-<a href="//www.blogmura.com/" target="_blank"><img src="//www.blogmura.com/img/www88_31.gif" width="88" height="31" alt="ブログランキング・にほんブログ村へ"></a>
-<a href="//blogram.jp/users/agent/vote/?uid=116487#ref=vote" target="_blank"><img src="//widget.blogram.jp/images/bgButton1_whi.gif" alt="blogram投票ボタン" width="88" height="31"></a>
-<a href="//blog.with2.net/link.php?1028897" target="_blank"><img src="/wp-content/themes/chabin/image/banner_13.gif" width="80" height="15" alt="人気ブログランキングへ"></a>
-</figure>
+    <aside>
+      <div class="sidebar__title">Others</div>
+      <figure>
+        <!--<a href="//feedmeter.net/detail.php?r=http%3A%2F%2Fmae.chab.in%2Ffeed" target="_blank"><img src="//feedmeter.net/image.php?r=http%3A%2F%2Fmae.chab.in%2Ffeed" width="60" height="36" alt="フィードメーター - Mae's Blog"></a>-->
+        <a href="//www.blogmura.com/" target="_blank"><img src="//www.blogmura.com/img/www88_31.gif" width="88" height="31" alt="ブログランキング・にほんブログ村へ"></a>
+        <a href="//blogram.jp/users/agent/vote/?uid=116487#ref=vote" target="_blank"><img src="//widget.blogram.jp/images/bgButton1_whi.gif" alt="blogram投票ボタン" width="88" height="31"></a>
+        <a href="//blog.with2.net/link.php?1028897" target="_blank"><img src="/wp-content/themes/chabin/image/banner_13.gif" width="80" height="15" alt="人気ブログランキングへ"></a>
+      </figure>
 
-<div class="sidebar__widget_ad">
-<div>スポンサードリンク</div>
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- mae's blog（サイドバー下） -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:300px;height:250px"
-     data-ad-client="ca-pub-6331923403728737"
-     data-ad-slot="2944739979"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-</div>
-</aside>
+      <div class="sidebar__widget_ad">
+        <div>スポンサードリンク</div>
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <!-- mae's blog（サイドバー下） -->
+        <ins class="adsbygoogle"
+          style="display:inline-block;width:300px;height:250px"
+          data-ad-client="ca-pub-6331923403728737"
+          data-ad-slot="2944739979"></ins>
+          <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+      </div>
+    </aside>
 
-</div><!-- #sidebar_sub -->
+  </div><!-- #sidebar_sub -->
 </div><!-- #sidebar -->
