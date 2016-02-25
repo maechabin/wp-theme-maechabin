31c6ab5f698581f876075b6fe3e607b188eaeff3
maechabin <mail@chab.in>
2016-02-25 22:11:27 +0900

    changed ad

--- single.php
+++ single.php
@@ -29,7 +29,7 @@ while(have_posts()):the_post();
 
   <aside class="ad-single1">
     <div class="ad-single1__text">
-      <iframe src="http://rcm-fe.amazon-adsystem.com/e/cm?t=wanderer-22&o=9&p=48&l=ur1&category=smp&banner=0MA5NHHMPDWVFPHE0CG2&f=ifr" width="728" height="90" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>
+      <iframe src="http://rcm-fe.amazon-adsystem.com/e/cm?t=wanderer-22&o=9&p=48&l=ur1&category=businessbooks&banner=1HV6T76ZW23W3RGQ36R2&f=ifr" width="728" height="90" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>
       <!--<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>-->
       <!-- mae’s blog（テキスト） -->
       <!--<ins class="adsbygoogle"
