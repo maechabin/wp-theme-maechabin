5aa04ecb7e9e51ed6a64d1dc07c2f0c20754b9d2
maechabin <mail@chab.in>
2016-04-14 21:54:41 +0900

    sidebar: bug fix

--- header.php
+++ header.php
@@ -63,7 +63,7 @@
 
   <link rel="shortcut icon" type="image/vnd.microsoft.icon" href="/wp-content/themes/chabin/favicon.ico">
   <link rel="icon" type="image/vnd.microsoft.icon" href="/wp-content/themes/chabin/favicon.ico">
-  <link rel="stylesheet" href="/wp-content/themes/chabin/assets/style-7dd4e4b31720fe69a9550f61155588e1.css" media="all">
+  <link rel="stylesheet" href="/wp-content/themes/chabin/assets/style-1ff2482e4497f36d2f322ee31fef38fc.css" media="all">
   <link rel="alternate" type="application/rss+xml" title="RSS" href="<?php bloginfo('rdf_url'); ?>">
   <?php wp_head(); ?>
   <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
