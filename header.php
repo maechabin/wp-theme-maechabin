17859ac00ef87dfe27ce800953206e9b35de89d0
maechabin <mail@chab.in>
2016-04-14 21:45:36 +0900

    css修正

--- header.php
+++ header.php
@@ -63,7 +63,7 @@
 
   <link rel="shortcut icon" type="image/vnd.microsoft.icon" href="/wp-content/themes/chabin/favicon.ico">
   <link rel="icon" type="image/vnd.microsoft.icon" href="/wp-content/themes/chabin/favicon.ico">
-  <link rel="stylesheet" href="/wp-content/themes/chabin/assets/style-948cb995e46209e19d0e0a6cbf3d04b5.css" media="all">
+  <link rel="stylesheet" href="/wp-content/themes/chabin/assets/style-7dd4e4b31720fe69a9550f61155588e1.css" media="all">
   <link rel="alternate" type="application/rss+xml" title="RSS" href="<?php bloginfo('rdf_url'); ?>">
   <?php wp_head(); ?>
   <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
