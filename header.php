d0061aba086a837514c4f3af7e208ff0b93581fd
maechabin <mail@chab.in>
2016-10-21 01:47:33 +0900

    modify index style

--- header.php
+++ header.php
@@ -63,7 +63,7 @@
 
   <link rel="shortcut icon" type="image/vnd.microsoft.icon" href="/wp-content/themes/chabin/favicon.ico">
   <link rel="icon" type="image/vnd.microsoft.icon" href="/wp-content/themes/chabin/favicon.ico">
-  <link rel="stylesheet" href="/wp-content/themes/chabin/assets/style-52a24fc9d735d3bd5e09537a80e38962.css" media="all">
+  <link rel="stylesheet" href="/wp-content/themes/chabin/assets/style-0625a7ebaf8abfafa5545dde0975df6f.css" media="all">
   <link rel="alternate" type="application/rss+xml" title="RSS" href="<?php bloginfo('rdf_url'); ?>">
   <?php wp_head(); ?>
 </head>
--- index.php
+++ index.php
@@ -31,19 +31,32 @@ while(have_posts()):the_post();
 ?>
 <!-- ▼section▼ -->
 <section class="post post-box">
-<h1 id="post-<?php the_ID(); ?>" class="post-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-<ul class="postmetadata-ul postmetadata">
-  <li class="postmetadata-li"><i class="fa fa-archive"></i> <?php the_category(', '); ?></li>
-  <?php /*<li class="postmetadata-li"><?php the_tags(); ?></li>*/ ?>
-  <?php /*<li class="postmetadata-li"><a href="http://b.hatena.ne.jp/entry/<?php the_permalink(); ?>" target="_blank"><img src="http://b.hatena.ne.jp/entry/image/<?php the_permalink(); ?>" alt="" /></a></li>*/ ?>
-  <li class="postmetadata-li"><?php edit_post_link('編集'); ?></li>
-</ul>
-<ul class="cb-share post__sharebutton" title="<?php the_permalink(); ?>">
-  <?php /*<li><i class="fa fa-comments-o"></i> <?php comments_popup_link('0','1','%'); ?></li>*/ ?>
-  <li class="cb-hb"><a href="//b.hatena.ne.jp/entry/mae.chab.in/archives/<?php the_ID(); ?>" target="_blank"><b>B!</b> <span></span></a></li>
-  <?php /*<li class="cb-tw"><a href="//twitter.com/intent/tweet?text=<?php the_title(); ?> <?php the_permalink(); ?> @maechabinさんから" target="_blank"><i class="fa fa-twitter"></i><span></span></a></li> */ ?>
-  <li class="cb-fb"><a href="//www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" target="_blank" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>', 'new', 'width=500,height=300');return false;"><i class="fa fa-facebook"></i><span></span></a></li>
-</ul>
+  <h1 id="post-<?php the_ID(); ?>" class="post-title">
+    <a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+  </h1>
+  <ul class="postmetadata-ul postmetadata">
+    <li class="postmetadata-li">
+      <i class="fa fa-archive"></i> <?php the_category(', '); ?>
+    </li>
+    <li class="postmetadata-li"><?php the_tags('#', ', #'); ?></li>
+    <li class="postmetadata-li">
+      <?php edit_post_link('編集'); ?>
+    </li>
+  </ul>
+  <ul class="cb-share post__sharebutton" title="<?php the_permalink(); ?>">
+    <?php /*<li><i class="fa fa-comments-o"></i> <?php comments_popup_link('0','1','%'); ?></li>*/ ?>
+    <li class="cb-hb">
+      <a href="//b.hatena.ne.jp/entry/mae.chab.in/archives/<?php the_ID(); ?>" target="_blank">
+        <b>B!</b> <span></span>
+      </a>
+    </li>
+    <?php /*<li class="cb-tw"><a href="//twitter.com/intent/tweet?text=<?php the_title(); ?> <?php the_permalink(); ?> @maechabinさんから" target="_blank"><i class="fa fa-twitter"></i><span></span></a></li> */ ?>
+    <li class="cb-fb">
+      <a href="//www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" target="_blank" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>', 'new', 'width=500,height=300');return false;">
+        <i class="fa fa-facebook"></i><span></span>
+      </a>
+    </li>
+  </ul>
 </section>
 <!-- ▲section▲ -->
 
