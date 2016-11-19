680ba624ed9eea090dd8513879e488d91c522393
maechabin <mail@chab.in>
2016-11-19 18:10:30 +0900

    modify: sidebar

--- header.php
+++ header.php
@@ -63,7 +63,7 @@
 
   <link rel="shortcut icon" type="image/vnd.microsoft.icon" href="/wp-content/themes/chabin/favicon.ico">
   <link rel="icon" type="image/vnd.microsoft.icon" href="/wp-content/themes/chabin/favicon.ico">
-  <link rel="stylesheet" href="/wp-content/themes/chabin/assets/style-8dc9fa7d77e96a88e390f3d493da2ee3.css" media="all">
+  <link rel="stylesheet" href="/wp-content/themes/chabin/assets/style-ca4a54ed68a6b219643e4a209355dedd.css" media="all">
   <link rel="alternate" type="application/rss+xml" title="RSS" href="<?php bloginfo('rdf_url'); ?>">
   <?php wp_head(); ?>
 </head>
--- sidebar.php
+++ sidebar.php
@@ -2,6 +2,24 @@
   <div id="sidebar_sub" class="sidebar__sub">
 
     <div class="sidebar__box">
+      <div>
+        <figure class="sidebar__author-photo">
+          <img src="/wp-content/uploads/2015/11/maechabin_400.png" width="128" height="128" alt="maechabin / Takanori Maeda">
+        </figure>
+        <p class="sidebar__author-name">Takanori Maeda</p>
+
+        <p class="sidebar__author-social">
+          <a href="//github.com/maechabin" target="_blank" onclick="ga('send', 'event', 'sidebar', 'click', 'about-github');"><i class="fa fa-github-alt"></i></a>
+          <a href="//twitter.com/maechabin" target="_blank" onclick="ga('send', 'event', 'sidebar', 'click', 'about-twitter');"><i class="fa fa-twitter"></i></a>
+          <a href="//www.facebook.com/maechabin" target="_blank" onclick="ga('send', 'event', 'sidebar', 'click', 'about-facebook');"><i class="fa fa-facebook"></i></a>
+          <a href="//plus.google.com/+TakanoriMaeda0_0/" target="_blank" onclick="ga('send', 'event', 'sidebar', 'click', 'about-google+');"><i class="fa fa-google-plus"></i></a>
+          <a href="//maechabin.tumblr.com/" target="_blank" onclick="ga('send', 'event', 'sidebar', 'click', 'about-tumblr');"><i class="fa fa-tumblr"></i></a>
+          <a href="https://www.instagram.com/maechabin/" target="_blank" onclick="ga('send', 'event', 'sidebar', 'click', 'about-insta');"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+          <a href="//www.muzie.ne.jp/artist/a024945/" target="_blank" onclick="ga('send', 'event', 'sidebar', 'click', 'about-muzie');"><i class="fa fa-music"></i></a>
+        </p>
+
+        <p class="sidebar__author-description">フロントエンドエンジニア。JavaScript、React、UI/UXデザインなどが専門分野。現在はネット広告企業でマーケティング担当。趣味はカメラ、ピアノ、作曲。</p>
+      </div>
       <aside>
         <div class="sidebar__widget_ad sidebar__ad1">
           <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
@@ -13,23 +31,6 @@
           <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
         </div>
       </aside>
-
-      <figure class="sidebar__author-photo">
-        <img src="/wp-content/uploads/2015/11/maechabin_400.png" width="128" height="128" alt="maechabin / Takanori Maeda">
-      </figure>
-      <p class="sidebar__author-name">Takanori Maeda</p>
-
-      <p class="sidebar__author-social">
-        <a href="//github.com/maechabin" target="_blank" onclick="ga('send', 'event', 'sidebar', 'click', 'about-github');"><i class="fa fa-github-alt"></i></a>
-        <a href="//twitter.com/maechabin" target="_blank" onclick="ga('send', 'event', 'sidebar', 'click', 'about-twitter');"><i class="fa fa-twitter"></i></a>
-        <a href="//www.facebook.com/maechabin" target="_blank" onclick="ga('send', 'event', 'sidebar', 'click', 'about-facebook');"><i class="fa fa-facebook"></i></a>
-        <a href="//plus.google.com/+TakanoriMaeda0_0/" target="_blank" onclick="ga('send', 'event', 'sidebar', 'click', 'about-google+');"><i class="fa fa-google-plus"></i></a>
-        <a href="//maechabin.tumblr.com/" target="_blank" onclick="ga('send', 'event', 'sidebar', 'click', 'about-tumblr');"><i class="fa fa-tumblr"></i></a>
-        <a href="https://www.instagram.com/maechabin/" target="_blank" onclick="ga('send', 'event', 'sidebar', 'click', 'about-insta');"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-        <a href="//www.muzie.ne.jp/artist/a024945/" target="_blank" onclick="ga('send', 'event', 'sidebar', 'click', 'about-muzie');"><i class="fa fa-music"></i></a>
-      </p>
-
-      <p class="sidebar__author-description">フロントエンドエンジニア & デザイナー。JavaScript、React、UI/UXデザインなどが専門分野。現在はネット広告企業でマーケティング担当。趣味はカメラ、ピアノ、作曲。</p>
     </div>
 
     <div class="mobile_only">
