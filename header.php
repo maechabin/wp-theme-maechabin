780be09099f9d011b2dc52d51285894616dc8eef
maechabin <mail@chab.in>
2016-12-05 23:10:39 +0900

    add stylelint

--- header.php
+++ header.php
@@ -63,7 +63,7 @@
 
   <link rel="shortcut icon" type="image/vnd.microsoft.icon" href="/wp-content/themes/chabin/favicon.ico">
   <link rel="icon" type="image/vnd.microsoft.icon" href="/wp-content/themes/chabin/favicon.ico">
-  <link rel="stylesheet" href="/wp-content/themes/chabin/assets/style-a1f01f57d1efa18b27841f38b92df82a.css" media="all">
+  <link rel="stylesheet" href="/wp-content/themes/chabin/assets/style-ec664694164e3db308719d8f6ca108bd.css" media="all">
   <link rel="alternate" type="application/rss+xml" title="RSS" href="<?php bloginfo('rdf_url'); ?>">
   <?php wp_head(); ?>
 </head>
