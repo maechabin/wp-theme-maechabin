232d87154d49ad2232054eb7b3dc4fe1faf33a62
maechabin <mail@chab.in>
2017-03-18 22:36:14 +0900

    広告ユニット修正

--- single.php
+++ single.php
@@ -28,26 +28,18 @@ while(have_posts()):the_post();
   </header>
 
   <div class="ad-single1">
-    <div class="pc_only">
-      <div class="ad-single1__text">
-        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <!-- mae's blog（記事上728-90） -->
-        <ins class="adsbygoogle"
-             style="display:inline-block;width:728px;height:90px"
-             data-ad-client="ca-pub-6331923403728737"
-             data-ad-slot="3358669171"></ins>
-        <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
-        <!--<ins class="adsbygoogle"
-             style="display:inline-block;width:468px;height:15px"
-             data-ad-client="ca-pub-6331923403728737"
-             data-ad-slot="3830887178"></ins>
-        <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>-->
-      </div>
-    </div>
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- maesblog（記事上レスポンシブ） -->
+    <ins class="adsbygoogle"
+         style="display:block"
+         data-ad-client="ca-pub-6331923403728737"
+         data-ad-slot="3040249173"
+         data-ad-format="auto"></ins>
+    <script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+  </div>
 
-    <div class="ad-single1__banner">
 <?php
 $iPod    = stripos($_SERVER['HTTP_USER_AGENT'],"iPod");
 $iPhone  = stripos($_SERVER['HTTP_USER_AGENT'],"iPhone");
@@ -56,33 +48,11 @@ $Android = stripos($_SERVER['HTTP_USER_AGENT'],"Android");
 $webOS   = stripos($_SERVER['HTTP_USER_AGENT'],"webOS");
 
 if ($iPod || $iPhone || $iPad) {
-?>
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- mae's blog（記事上モバイルバナー） -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:320px;height:100px"
-     data-ad-client="ca-pub-6331923403728737"
-     data-ad-slot="7675360770"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-<?php
+
 } else {
-?>
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- mae's blog（記事上モバイルバナー） -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:320px;height:100px"
-     data-ad-client="ca-pub-6331923403728737"
-     data-ad-slot="7675360770"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-<?php
+
 }
 ?>
-    </div>
-  </div>
 
   <div class="entry">
   <?php the_content(); ?>
@@ -93,30 +63,16 @@ if ($iPod || $iPhone || $iPad) {
   <?php wp_link_pages('before=&after=&next_or_number=number&pagelink=%'); ?>
   </div>
 
-  <section id="related-posts">
-    <h1 id="related-posts-title"><i class="fa fa-files-o"></i> 関連記事</h1>
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <ins class="adsbygoogle"
-         style="display:block"
-         data-ad-format="autorelaxed"
-         data-ad-client="ca-pub-6331923403728737"
-         data-ad-slot="3210202778"></ins>
-    <script>
-         (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
-  </section>
-
   <div class="ad-single2">
     <div class="sd-single2__banner">
-      <p class="ad-single2__title">スポンサードリンク</p>
       <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-      <!-- mae’s blog（記事下） -->
       <ins class="adsbygoogle"
-           style="display:inline-block;width:336px;height:280px;"
+           style="display:block"
+           data-ad-format="autorelaxed"
            data-ad-client="ca-pub-6331923403728737"
-           data-ad-slot="4144613456"></ins>
+           data-ad-slot="3210202778"></ins>
       <script>
-      (adsbygoogle = window.adsbygoogle || []).push({});
+           (adsbygoogle = window.adsbygoogle || []).push({});
       </script>
     </div>
   </div>
