5e84861b672410c8a113509b2d240d69fb0f80b7
maechabin <mail@chab.in>
2017-07-06 22:15:17 +0900

    本文pのmargin-bottom修正

--- header.php
+++ header.php
@@ -66,7 +66,7 @@
   <link rel="canonical" href="https://mae.chab.in<?php echo $path ?>">
   <link rel="shortcut icon" type="image/vnd.microsoft.icon" href="/wp-content/themes/chabin/favicon.ico">
   <link rel="icon" type="image/vnd.microsoft.icon" href="/wp-content/themes/chabin/favicon.ico">
-  <link rel="stylesheet" href="/wp-content/themes/chabin/assets/style-81907d46a88c2c0e4f8c3078eebf897c.css" media="all">
+  <link rel="stylesheet" href="/wp-content/themes/chabin/assets/style-ef0316dfde867baac8706ad1f771920f.css" media="all">
   <link rel="alternate" type="application/rss+xml" title="RSS" href="<?php bloginfo('rdf_url'); ?>">
   <?php wp_head(); ?>
 </head>
