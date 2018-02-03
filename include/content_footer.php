d1f6cc3f83b14f2957165419fc40a48bee9cc742
maechabin <mail@chab.in>
2018-02-03 18:01:10 +0900

    #9: 外部ファイル化

---
+++ include/content_footer.php
@@ -0,0 +1,54 @@
+<footer>
+  <div class="footer">
+    <div id="footer_sub" class="footer__sub">
+      <ul class="footer__share-button">
+        <li class="footer__share-button--list">
+          <a href="https://github.com/maechabin" target="_blank" class="footer__share-button--gh">
+            <i class="fa fa-github"></i>
+          </a>
+        <li>
+        <li class="footer__share-button--list">
+          <a href="https://twitter.com/maechabin" target="_blank" class="footer__share-button--tw">
+            <i class="fa fa-twitter"></i>
+          </a>
+        </li>
+        <li class="footer__share-button--list">
+          <a href="https://www.facebook.com/maechabin" target="_blank" class="footer__share-button--fb">
+            <i class="fa fa-facebook"></i>
+          </a>
+        </li>
+        <li class="footer__share-button--list">
+          <a href="https://plus.google.com/105642680110503345013?rel=author" target="_blank" class="footer__share-button--gp">
+            <i class="fa fa-google-plus"></i>
+          </a>
+        </li>
+      </ul>
+
+      <address class="footer__credit">
+        <strong><a href="/">maesblog</a></strong>
+        &copy; 2018 maechabin
+      </address>
+
+    </div><!-- #footer_sub -->
+  </div>
+
+  <div id="footer_bar" class="footer__bar">
+    <ul>
+      <li class="footer__bar-link footer__bar-link--left" id="footer__bar_home">
+        <a href="/">
+          <i class="fa fa-home"></i>ホーム
+        </a>
+      </li>
+      <li class="footer__bar-link footer__bar-link--center footer__style--hidden" id="footer__bar__agenda-link">
+        <a href="#agenda">
+          <i class="fa fa-book"></i>目次
+        </a>
+      </li>
+      <li class="footer__bar-link footer__bar-link--right">
+        <a href="#TOP">
+          <i class="fa fa-chevron-circle-up"></i>トップ
+        </a>
+      </li>
+    </ul>
+  </div>
+</footer>
