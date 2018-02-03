62505e14d86e166cfaec983c8a79e2870ec80e1a
maechabin <mail@chab.in>
2018-02-03 17:33:51 +0900

    #9: 外部ファイル化

---
+++ include/notfound.php
@@ -0,0 +1,15 @@
+<div class='not-found'>
+  <h2 class="entry-h3" style="text-align: center;">Not Found</h2>
+  <p style="font-size: 16px;text-align: center;">該当する記事はありませんでした。</p>
+  <div style="text-align: center;margin-top: 40px;">
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- mae's blog（Page/index用） -->
+    <ins class="adsbygoogle"
+      style="display:inline-block;width:468px;height:60px"
+      data-ad-client="ca-pub-6331923403728737"
+      data-ad-slot="8443865971"></ins>
+    <script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+  </div>
+</div>
