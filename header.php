16785fe26c8ce134e283c26939477ed057f53a26
maechabin <mail@chab.in>
2018-03-05 00:47:37 +0900

    #37: v2.6.4

--- header.php
+++ header.php
@@ -63,11 +63,7 @@
   <link rel="canonical" href="https://mae.chab.in<?php echo $path ?>">
   <link rel="shortcut icon" type="image/vnd.microsoft.icon" href="/wp-content/themes/chabin/favicon.ico">
   <link rel="icon" type="image/vnd.microsoft.icon" href="/wp-content/themes/chabin/favicon.ico">
-<<<<<<< HEAD
-  <link rel="stylesheet" href="/wp-content/themes/chabin/assets/style-7d109902b18ce1cbc8d4ca8a14e5067d.css" media="all">
-=======
-  <link rel="stylesheet" href="/wp-content/themes/chabin/assets/style-7d109902b18ce1cbc8d4ca8a14e5067d.css" media="all">
->>>>>>> develop
+  <link rel="stylesheet" href="/wp-content/themes/chabin/assets/style-8a8b0e932e959515ee0c7d0fe833cd0c.css" media="all">
   <link rel="alternate" type="application/rss+xml" title="RSS" href="<?php bloginfo('rdf_url'); ?>">
   <?php wp_head(); ?>
 </head>
