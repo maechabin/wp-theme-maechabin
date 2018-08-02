363314ccb0f87f38d73b95ecb5106276bea44700
maechabin <mitoppo@gmail.com>
2018-08-02 21:57:55 +0900

    #55: RAIL対応

--- include/content_header.php
+++ include/content_header.php
@@ -1,43 +1,56 @@
 <header id="header_bar" class="header cb-header">
-  <div id="header_bar_inner" class="header__inner">
+  <h1 class="header__title">
+    <a href="/"><i style="color:#fff;"></i></a>
+    <a href="/" class="header__title_link">maesblog</a>
+  </h1>
 
-    <h1 class="header__title">
-      <a href="/"><i style="color:#fff;"></i></a>
-      <a href="/">maesblog</a>
-    </h1>
+  <div class="header__search">
+    <form role="search" method="get" id="header__search-form" action="<?php bloginfo('url') ?>" >
+      <input type="text" value="<?php echo get_search_query(); ?>" name="s" placeholder="ブログ記事を検索" class="header__search-text"><button type="submit" class="header__search-button"><i class="fa fa-search"></i></button>
+    </form>
+  </div>
 
-    <div class="header__search">
-      <form role="search" method="get" id="header__search-form" action="<?php bloginfo('url') ?>" >
-        <input type="text" value="<?php echo get_search_query(); ?>" name="s" placeholder="ブログ記事を検索" class="header__search-text"><button type="submit" class="header__search-button"><i class="fa fa-search"></i></button>
-      </form>
-    </div>
+  <div class="header__button_search">
+    <i class="fa fa-search"></i>
+  </div>
 
-    <div class="header__button_search">
-      <i class="fa fa-search"></i>
-    </div>
-    <div class="header__share">
-      <a href="https://feedly.com/i/subscription/feed/https://mae.chab.in/feed" target="_blank" rel="noopener"><i class="fa fa-rss"></i></a>
-    </div>
+  <div class="header__share">
+    <a href="https://feedly.com/i/subscription/feed/https://mae.chab.in/feed" target="_blank" rel="noopener"><i class="fa fa-rss"></i></a>
+  </div>
 
+  <div class="header__search_mobile <?php if (is_search()) echo 'header__display' ?>">
+    <p class="header__button_back"><i class="fa fa-chevron-left"></i></p>
+    <form role="search" method="get" class="header__search-form_mobile" action="<?php bloginfo('url') ?>" >
+      <input type="text" value="<?php echo get_search_query(); ?>" name="s" placeholder="ブログ記事を検索" class="header__search-text_mobile"><button type="submit" class="header__search-button_mobile"><i class="fa fa-search"></i></button>
+    </form>
   </div>
 </header>
 <!-- ▲header▲ -->
-
-<div class="header__search_mobile <?php if (is_search()) echo 'header__display' ?>">
-  <p class="header__button_back"><i class="fa fa-chevron-left"></i></p>
-  <form role="search" method="get" class="header__search-form_mobile" action="<?php bloginfo('url') ?>" >
-    <input type="text" value="<?php echo get_search_query(); ?>" name="s" placeholder="ブログ記事を検索" class="header__search-text_mobile"><button type="submit" class="header__search-button_mobile"><i class="fa fa-search"></i></button>
-  </form>
-</div>
 
 <?php
   $all = get_category(31)->category_count + get_category(33)->category_count + get_category(32)->category_count;
 ?>
 <nav class="category">
   <ul class="category__list">
-    <li class="<?php echo current_category('all'); ?>"><a href="/">All<span>（<?php echo $all; ?>）</span></a></li>
-    <li class="<?php echo current_category('Web技術'); ?>"><a href="/archives/category/tech">Web技術<span>（<?php echo get_category(31)->category_count; ?>）</span></a></li>
-    <li class="<?php echo current_category('出来事'); ?>"><a href="/archives/category/event">出来事<span>（<?php echo get_category(33)->category_count; ?>）</span></a></li>
-    <li class="<?php echo current_category('所感'); ?>"><a href="/archives/category/impression">所感<span>（<?php echo get_category(32)->category_count; ?>）</span></a></li>
+    <li class="category__list_li <?php echo current_category('all'); ?>">
+      <a class="category__link" href="/">All
+        <span class="category__name">（<?php echo $all; ?>）</span>
+      </a>
+    </li>
+    <li class="category__list_li <?php echo current_category('Web技術'); ?>">
+      <a class="category__link"  href="/archives/category/tech">Web技術
+        <span class="category__name">（<?php echo get_category(31)->category_count; ?>）</span>
+      </a>
+    </li>
+    <li class="category__list_li <?php echo current_category('出来事'); ?>">
+      <a class="category__link"  href="/archives/category/event">出来事
+        <span class="category__name">（<?php echo get_category(33)->category_count; ?>）</span>
+      </a>
+    </li>
+    <li class="category__list_li <?php echo current_category('所感'); ?>">
+      <a class="category__link"  href="/archives/category/impression">所感
+        <span class="category__name">（<?php echo get_category(32)->category_count; ?>）</span>
+      </a>
+    </li>
   </ul>
 </nav>
--- index.php
+++ index.php
@@ -17,7 +17,12 @@
             <!-- ▼section▼ -->
             <section class="post">
               <h1 id="post-<?php the_ID(); ?>" class="post__title">
-                <a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+                <a
+                  class="post__title_link"
+                  href="<?php the_permalink(); ?>"
+                >
+                  <?php the_title(); ?>
+                </a>
               </h1>
 
               <div class="post__meta">
@@ -28,7 +33,12 @@
                   <li class="post__meta-li">
                     <i class="fa fa-calendar" aria-hidden="true"></i>
                     <time pubdate class="post__date">
-                      <a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_date('Y年m月d日'); ?></a>
+                      <a
+                        class="post__date_link"
+                        href="<?php the_permalink(); ?>"
+                      >
+                        <?php the_date('Y年m月d日'); ?>
+                      </a>
                     </time>
                   </li>
                   <?php /*<li class="postmetadata-li post__tags"><?php the_tags('#', ', #'); ?></li>*/ ?>
@@ -37,43 +47,47 @@
                   </li>
                 </ul>
 
-                <ul class="post__socialbtn-ul cb-share" title="<?php the_permalink(); ?>">
+                <ul class="post__socialbtn-ul">
                   <?php /*<li><i class="fa fa-comments-o"></i> <?php comments_popup_link('0','1','%'); ?></li>*/ ?>
-                  <li class="post__socialbtn-li cb-tw">
+                  <li class="post__socialbtn-li">
                     <a
+                      class="post__socialbtn-link"
                       href="//twitter.com/intent/tweet?text=<?php the_title(); ?> <?php the_permalink(); ?> @maechabinさんから"
                       target="_blank"
                       rel="noopener"
                     >
-                      <i class="fa fa-twitter"></i> <span></span>
+                      <i class="fa fa-twitter"></i>
                     </a>
                   </li>
-                  <li class="post__socialbtn-li cb-fb">
+                  <li class="post__socialbtn-li">
                     <a
+                      class="post__socialbtn-link"
                       href="//www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>"
                       target="_blank"
                       rel="noopener"
                       onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>', 'new', 'width=500,height=300');return false;"
                     >
-                      <i class="fa fa-facebook"></i><span></span>
+                      <i class="fa fa-facebook"></i>
                     </a>
                   </li>
-                  <li class="post__socialbtn-li cb-hb">
+                  <li class="post__socialbtn-li">
                     <a
+                      class="post__socialbtn-link"
                       href="//b.hatena.ne.jp/entry/<?php the_permalink(); ?>"
                       target="_blank"
                       rel="noopener"
                     >
-                      <b>B!</b> <span></span>
+                      <b>B!</b>
                     </a>
                   </li>
-                  <li class="post__socialbtn-li cb-pk">
+                  <li class="post__socialbtn-li">
                     <a
+                      class="post__socialbtn-link"
                       href="//getpocket.com/edit?url=<?php the_permalink(); ?>"
                       target="_blank"
                       rel="noopener"
                     >
-                      <i class="fa fa-get-pocket"></i> <span></span>
+                      <i class="fa fa-get-pocket"></i>
                     </a>
                   </li>
                 </ul>
